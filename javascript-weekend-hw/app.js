b2c0050c31a1f8c3d1d9a9dd49382ba8b47a2202
aridoza <mendozaarielr@gmail.com>
2019-09-22 20:36:41 -0400

    weekend hw part 1 complete

---
+++ javascript-weekend-hw/app.js
@@ -0,0 +1,32 @@
+// I. Variables & Datatypes
+
+// 1. How do we assign a value to a variable?
+// use the assignment operator '=' to assign a value
+// ex: const thing1 = 'the thing';
+
+// 2. How do we change the value of a variable?
+// we use the assignment operator; variables created with 'let' can be changed to different data types, while 
+// variables created with 'const' cannot
+// ex: let someNum = 5;
+// someNum = 10;
+
+// 3. How do we assign an existing variable to a new variable?
+// we can use the assignment operator '=' to assign the value of the existing variable to a new variable
+// ex: let num1 = 7;
+// let num2 = num1;  // num2 is now equal to 7;
+
+// 4. Remind me, what are declare, assign, and define?
+// declare a variable using either the 'let' or 'const' keyword
+// assign a value to the variable using the assignment operator '='
+// define the data type of the variable (string, number, boolean, array, object) or initialize to null
+
+// 5. What is pseudocoding and why should you do it?
+// pseudocoding involves writing out what you want the code to do, using a combination of code and human-readable language
+// it helps plan out how your application will work by breaking up your code into smaller chunks, and helps save time when actually coding
+// because ideas are planned out better
+// it should describe the entire logic of a problem, so you're ultimately transposing it into actual code
+
+// 6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+// Should spend about 75-90% of your time thinking about/planning how you're going to solve a problem, and about 10-25% of your time
+// actually typing in the code to solve it
+
